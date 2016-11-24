8d1b07678afd024284b6b12dc92af33ca4e7a110
Mattycodes <matt.nick.fagioli@gmail.com>
2016-11-24 18:28:20 -0500

    Fixes bug.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def destroy
+  	session.clear
+  	redirect_to('/')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-	devise_for :users, controllers: {
-		sessions: "users/sessions"
-	}	
-
+	devise_for :users
 	resources :posts
 	get "users/:id",					 to: "users#show"
 	get "hashtags/:hashtag",   to: "hashtags#show",      as: :hashtag
 	get "hashtags",            to: "hashtags#index",     as: :hashtags
-
 	root "posts#index"
 end
