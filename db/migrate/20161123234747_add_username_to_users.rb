0e19ebddf0d63b551324f11bc37580e1f0d5f529
RicardoG2016 <gomez.r@live.com>
2016-11-23 18:00:48 -0600

    added additional columns to user table

---
+++ db/migrate/20161123234747_add_username_to_users.rb
@@ -0,0 +1,7 @@
+class AddUsernameToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :username, :string
+    add_column :users, :fullname, :string
+    add_column :users, :bio, :text
+  end
+end
