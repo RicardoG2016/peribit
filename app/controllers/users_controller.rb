3ac48146000246892430266d1f0c1b6e9e46d33b
Mattycodes <matt.nick.fagioli@gmail.com>
2016-11-25 13:29:09 -0500

    Implements follower logic on the user page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,24 @@
 class UsersController < ApplicationController
   def show
+  	# binding.pry
+  	redirect_to('/') and return if !current_user
     @user = User.find(params[:id])
   end
 
   def destroy
   	session.clear
   	redirect_to('/')
+  end
+
+  def follow
+  	user = User.find(params[:id])
+  	current_user.follow!(user)
+  	redirect_to(:back)
+  end
+
+  def unfollow
+  	user = User.find(params[:id])
+  	current_user.unfollow!(user)
+  	redirect_to(:back)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 	devise_for :users
 	resources :posts
 	get "users/:id",					 to: "users#show"
+	post "users/:id/follow",	 to: "users#follow"
+	post "users/:id/unfollow", to: "users#unfollow"
 	get "hashtags/:hashtag",   to: "hashtags#show",      as: :hashtag
 	get "hashtags",            to: "hashtags#index",     as: :hashtags
 	root "posts#index"
