7617072c0d01b4ecd2c7943811a8afc01ab1331c
Mattycodes <matt.nick.fagioli@gmail.com>
2016-11-28 18:53:11 -0600

    Adds validations and corrects the delete method for replies.

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,7 @@ Rails.application.routes.draw do
 	post "posts/:id/like", 		 to: "posts#like_post",    as: :like
 	get "posts/:id/reply",     to: "replies#new"
 	post "posts/:id/reply",     to: "replies#create"
-
-  get "newsfeeds",           to: "newsfeeds#index",    as: :newsfeeds
+	delete "replies/:id",		   to: "replies#destroy"
   get "followers",           to: "followers#index",    as: :followers
   get "following",           to: "following#index",    as: :following
 	
