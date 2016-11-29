5a15690b7b203e4ac4b2429c6bd0d3f34b8ca446
Juan Rojas <jmr5150@gmail.com>
2016-11-28 19:03:34 -0600

    Adds paperclip.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_attached_file :avatar, styles: { large: "300x300#", medium: "150x150#", thumb: "75x75#" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
   acts_as_follower
   acts_as_followable
   acts_as_liker
