8337343cbb4f88e5766c2e6a9b44f83f86a02f5c
Mattycodes <matt.nick.fagioli@gmail.com>
2016-11-28 18:10:55 -0600

    Completes styling positions and corrects edit page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :replies
+  delete "users/:id/account", 	      to: "users#delete"
 	devise_for :users, controllers: {
 	  registrations: 'users/registrations'
 	}
