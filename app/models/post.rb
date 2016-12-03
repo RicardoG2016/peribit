eb63783ab05fdf6fd84b188812661402e2269611
Mattycodes <matt.nick.fagioli@gmail.com>
2016-12-02 22:00:19 -0600

    Adds dependency to replies.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   belongs_to :user
   acts_as_likeable
   acts_as_mentioner
-  has_many :replies
+  has_many :replies, dependent: :destroy
   scope :trending, -> { order("delete_at DESC").limit(10) }
   scope :feed, -> { order("created_at ASC")}
   has_attached_file :image, :styles => { large: "600x600>", medium: "300x300>", thumb: "150x150#"}
